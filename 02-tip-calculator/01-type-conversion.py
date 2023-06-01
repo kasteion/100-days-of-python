0ad9145d2e6196ba3cd243b249dceac0175fb9ae
Fredy Augusto Castellon Bedoya <castellonbedoya@gmail.com>
2023-06-01 15:25:17 -0600

    feat: day 2 - types and conversion - tip calculator

---
+++ 02-tip-calculator/01-type-conversion.py
@@ -0,0 +1,9 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+# Write your code below this line 👇
+first_digit = int(two_digit_number[:1])
+second_digit = int(two_digit_number[1:])
+print(str(first_digit + second_digit))
