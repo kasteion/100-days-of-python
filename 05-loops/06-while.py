8a63d6965ec46520f97fdb198a9bfdbcd45ceb4d
Fredy Augusto Castellon Bedoya <castellonbedoya@gmail.com>
2023-06-05 09:49:32 -0600

    feat: day 6 - functions

---
+++ 05-loops/06-while.py
@@ -0,0 +1,16 @@
+print("For with list")
+list_of_items = ["item 01", "item 02", "item 03", "item 04"]
+for item in list_of_items:
+    print(item)
+
+print("\n\nFor with range")
+a = 1
+b = 10
+for number in range(a, b):
+    print(number)
+
+print("\n\nWhile loop")
+i = 0
+while i < 10:
+    print(i)
+    i += 1
