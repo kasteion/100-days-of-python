962784d41e2351d2ed5a5bbdd4043952df80f856
Fredy Augusto Castellon Bedoya <castellonbedoya@gmail.com>
2023-07-04 23:10:17 -0600

    refactor: day 25 - mail merge and day 35 - quiz app refactor

--- 25-files-directories-paths/main.py
+++ 25-files-directories-paths/main.py
@@ -1,8 +1,8 @@
 
-with open("./input/letters/starting_letter.txt") as letter:
+with open("input/letters/starting_letter.txt") as letter:
     letter_format = letter.read()
 
-with open("./input/names/invited_names.txt") as names:
+with open("input/names/invited_names.txt") as names:
     for name in names:
         new_letter = letter_format.replace("[name]", name.strip())
         with open(f"./output/ready_to_send/letter_to_{name.strip().lower()}.txt", mode="w") as letter:
